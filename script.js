bc67189c6af5fbad5da006b9174ef753359e25c7
Arup_kr_das <arup80424@gmail.com>
2025-07-12 23:56:27 +0530

    add FAQ section and reasons to join; implement language selection dropdown; enhance styles and integrate Font Awesome

--- script.js
+++ script.js
@@ -9,3 +9,29 @@ nextBtn.addEventListener('click', () => {
 prevBtn.addEventListener('click', () => {
   slider.scrollBy({ left: -900, behavior: 'smooth' });
 });
+
+const faqItems = document.querySelectorAll('.faq-item');
+
+faqItems.forEach(item => {
+  const questionBtn = item.querySelector('.faq-question');
+  const answer = item.querySelector('.faq-answer');
+  const icon = questionBtn.querySelector('i');
+
+  questionBtn.addEventListener('click', () => {
+    const isOpen = answer.style.maxHeight;
+
+    // Close all
+    faqItems.forEach(otherItem => {
+      otherItem.querySelector('.faq-answer').style.maxHeight = null;
+      otherItem.querySelector('i').classList.replace('fa-times', 'fa-plus');
+    });
+
+    if (!isOpen) {
+      answer.style.maxHeight = answer.scrollHeight + "px";
+      icon.classList.replace('fa-plus', 'fa-times');
+    } else {
+      answer.style.maxHeight = null;
+      icon.classList.replace('fa-times', 'fa-plus');
+    }
+  });
+});
